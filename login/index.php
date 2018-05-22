abb29ac79bdd6c5319b9995263e8a4280a275d3f
MauriRojas <31770211+MauriRojas@users.noreply.github.com>
2018-05-22 00:52:24 -0300

    Login
    Added  login form.

---
+++ login/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>BSOD Information Center | Login</title>
+</head>
+
+<body>
+  <div class="login-form">
+    <form id="login" action="login" method="post">
+    <h1>BSOD</h1>
+    <h2>Information Center<h2>
+    <label for="username">Usuario</label>
+    <input id="username" type="text" name="username" required>
+    <label for="password">Contraseña</label>
+    <input id="password" type="password" name="password" required>
+    <input class="submit_button" type="submit" value="login" name="submit">
+  </form>
+  </div>
+</body>
+</html>
