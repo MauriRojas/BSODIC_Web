d3aa52a3d6847b9ffeff73d564935c0a60e3fb75
MauriRojas <31770211+MauriRojas@users.noreply.github.com>
2018-05-22 02:01:21 -0300

    Styled login form
    Login form styled.

--- login/index.php
+++ login/index.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link type="text/css" href="../styles/normalize.css" rel="stylesheet" media="all">
+  <link type="text/css" href="../styles/style.css" rel="stylesheet" media="all">
   <title>BSOD Information Center | Login</title>
 </head>
 
@@ -11,11 +13,9 @@
     <form id="login" action="login" method="post">
     <h1>BSOD</h1>
     <h2>Information Center<h2>
-    <label for="username">Usuario</label>
-    <input id="username" type="text" name="username" required>
-    <label for="password">Contraseña</label>
-    <input id="password" type="password" name="password" required>
-    <input class="submit_button" type="submit" value="login" name="submit">
+    <input id="username" type="text" name="username" placeholder="usuario" required autofocus>
+    <input id="password" type="password" name="password" placeholder="contraseña" required>
+    <input class="submit_button" type="submit" value="Iniciar sesión" name="submit">
   </form>
   </div>
 </body>
