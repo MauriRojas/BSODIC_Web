690655e99c6966ab93ed9bad8154614f16fbceaa
MauriRojas <31770211+MauriRojas@users.noreply.github.com>
2018-05-26 12:26:10 -0300

    Validate form and AJAX for register account
    Added validateForm function and implemented AJAX for register an account.

--- js/register.js
+++ js/register.js
@@ -1,15 +1,32 @@
-function checkSameInput() {
-  var pass = document.getElementById("pass");
-  var rpass = document.getElementById("rpass");
-  var submit = document.getElementById("enviar");
-  if (pass.value == rpass.value ) {
-    rpass.style.borderColor = "green";
-    submit.backgroundColor = "black";
-    submit.cursor = "default";
-    submit.disabled = false;
+function validateForm(form) {
+  var reason = "";
+  reason += validateSameInput(form.pass, form.rpass, "contraseña");
+  reason += validateSameInput(form.email, form.remail, "email");
+
+  if (reason != "") {
+    alert("Necesita corregir lo siguiente:\n" + reason);
   } else {
-    rpass.style.borderColor = "red";
-    submit.color = "green";
-    submit.disabled = true;
+    sendRegisterForm(this);
   }
+}
+
+function validateSameInput(textA, textB, inputType) {
+  if (textA = textB) {
+    return "";
+  } else {
+    return "Los campos de " + inputType + " deben coincidir.";
+  }
+}
+
+function sendRegisterForm(form) {
+  var parameters = "name=" + form.username.value + "&pass=" + form.pass.value + "&email=" + form.email.value + "&phone=" + form.phone.value;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      alert("Se ha registrado exitosamente!");
+    }
+  };
+  request.open("POST", "../serverside/register-account.php", true);
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  request.send(parameters);
 }
