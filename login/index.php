5f9ea30c47309a89071600a9138124e288185434
MauriRojas <31770211+MauriRojas@users.noreply.github.com>
2018-05-26 12:27:07 -0300

    Changed style file for login
    Now the login page has its own style file.

--- login/index.php
+++ login/index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link type="text/css" href="../styles/normalize.css" rel="stylesheet" media="all">
-  <link type="text/css" href="../styles/style.css" rel="stylesheet" media="all">
+  <link type="text/css" href="../styles/style-login.css" rel="stylesheet" media="all">
   <title>Iniciar sesión | BSOD Information Center</title>
 </head>
 
